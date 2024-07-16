487318c62127720654b4ccbc2635885277496390
Nicolas Jaros <njaros@z2r4p8.42lyon.fr>
2024-07-16 11:04:55 +0200

    print steps 20 firsts elem of the band only

--- app/Turing.hs
+++ app/Turing.hs
@@ -1,12 +1,14 @@
 module Turing where
 
 displayableBand :: String -> Char -> String
-displayableBand band blank = (takeWhile (/= blank) band) ++ [blank, blank]
+displayableBand band blank = take 20 band
+-- displayableBand band blank = (takeWhile (/= blank) band) ++ [blank, blank]
 
-printCurrent :: String -> Int -> IO ()
-printCurrent s i = do
-    let splitted = splitAt i s
-    putStrLn (fst splitted ++ "<" ++ [head (snd splitted)] ++ ">" ++ (tail (snd splitted)))
+printCurrent :: String -> Int -> String -> Char -> (String, Char, String) -> IO ()
+printCurrent band idx state read (b, c, d) = do
+    let splitted = splitAt idx band
+    putStrLn ("[" ++ fst splitted ++ "<" ++ [head (snd splitted)] ++ ">" ++ (tail (snd splitted)) ++ "]" ++ " (" ++ state ++ ", " ++ [read] ++ ") -> (" ++ b ++ ", " ++ [c] ++ ", " ++ d ++ ")")
+
 
 evaluate :: String -> Char -> [(String, [(Char, String, Char, Int)])] -> (Char, String, Char, Int)
 evaluate state read transitions = do
@@ -16,11 +18,15 @@ evaluate state read transitions = do
 applyModify :: String -> Int -> Char -> String
 applyModify band idx newChar = (take idx band) ++ [newChar] ++ (drop (idx + 1) band)
 
+toStringDirection :: Int -> String
+toStringDirection 1 = "RIGHT"
+toStringDirection (-1) = "LEFT"
+toStringDirection x = "Error"
+
 proceed :: String -> Int -> Char -> String -> [(String, [(Char, String, Char, Int)])] -> [String] -> IO ()
 proceed band idx blank state transitions finals = if state `elem` finals
-    then print (displayableBand band blank)
+    then putStrLn ("[" ++ displayableBand band blank ++ "]")
     else do
-        -- printCurrent (displayableBand band blank) idx
         let (a, b, c, d) = evaluate state (band !! idx) transitions
-        print (a, b, c, d)
+        printCurrent (displayableBand band blank) idx state (band !! idx) (b, c, (toStringDirection d))
         proceed (applyModify band idx c) (idx + d) blank b transitions finals
