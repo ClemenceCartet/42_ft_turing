d0446977f4ac0d6b2c710c70023edf17ed0d7d84
Clemence Cartet <ccartet@z2r2p8.42lyon.fr>
2024-07-18 17:38:45 +0200

    open file ok

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,28 @@
 import System.Environment
 import System.IO
 
+
+parseFile :: [String] => IO ()
+parseFile config = do
+    if head (head config) == '{'
+        then print("ok")
+        else print("no")
+
+readFromFile :: String -> IO String
+readFromFile filePath = do
+    handle <- openFile filePath ReadMode
+    hGetContents handle
+    -- hClose handle
+
 main :: IO ()
-
-
 main = do
     args <- getArgs
     mapM putStrLn args -- maps the function over the list and then sequences it
     if length args == 1
         then do
-            handle <- openFile (head args) ReadMode
-            contents <- hGetContents handle
-            putStrLn contents
-            hClose handle
+            contents <- readFromFile (head args)
+            -- putStrLn contents
+            let config = lines contents
+            parseFile config
+            -- print(length config)
         else putStrLn "You must pass your config file to this program."
