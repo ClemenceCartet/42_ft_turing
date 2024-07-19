073698b49c6dbe3551589204c547a3f6abd5e603
Clémence <clemence.cartet@geode.com>
2024-07-19 11:45:43 +0200

    checkArgs and printHelp

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,51 @@
-import System.Environment
+import System.Environment (getArgs, getProgName)
 import System.IO
+import System.Exit (exitSuccess)
 
 
-parseFile :: [String] => IO ()
+parseFile :: [String] -> IO ()
 parseFile config = do
     if head (head config) == '{'
-        then print("ok")
-        else print("no")
+        then print "ok"
+        else print "no"
 
 readFromFile :: String -> IO String
 readFromFile filePath = do
     handle <- openFile filePath ReadMode
     hGetContents handle
-    -- hClose handle
+    -- hClose handle 
+    -- return contents
+
+printHelp :: IO ()
+printHelp = do
+    program <- getProgName
+    putStrLn $ "usage: " ++ program ++ " [-h] jsonfile input\n\
+    \\n\
+    \positional arguments:\n\
+    \ jsonfile              json description of the machine\n\
+    \\n\
+    \ input                 input of the machine\n\
+    \\n\
+    \optional arguments:\n\
+    \ -h, --help            show this help message and exit"
+
+checkArgs :: [String] -> IO ()
+checkArgs args
+    | len == 0 = putStrLn "Empty input..." >> exitSuccess
+    | head args == "-h" || head args == "--help" = printHelp >> exitSuccess
+    | len == 2 = return ()
+    | otherwise = putStrLn "Incorrect number of arguments..." >> exitSuccess
+    where len = length args
 
 main :: IO ()
 main = do
     args <- getArgs
-    mapM putStrLn args -- maps the function over the list and then sequences it
-    if length args == 1
-        then do
-            contents <- readFromFile (head args)
-            -- putStrLn contents
-            let config = lines contents
-            parseFile config
-            -- print(length config)
-        else putStrLn "You must pass your config file to this program."
+    checkArgs args
+    let (jsonfile, input) = (args !! 0, args !! 1)
+
+    --         contents <- readFromFile (head args)
+    --         -- putStrLn contents
+    --         let config = lines contents
+    --         parseFile config
+    --         -- print(length config)
+    --     else putStrLn "You must pass your config file to this program."
