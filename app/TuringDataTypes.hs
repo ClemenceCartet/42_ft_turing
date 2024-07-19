bc89ee30525dd39176d4e276bcfb9d6006556158
pouet <git@mesoule.tamere>
2024-07-19 14:17:16 +0200

    redefinition of datatypes

--- app/TuringDataTypes.hs
+++ app/TuringDataTypes.hs
@@ -1,26 +1,14 @@
 module TuringDataTypes where
 
-data Transition = Transition Char String Char Int deriving (Show)
+data Transition = Transition { charRead :: Char
+                             , nextState :: String
+                             , replaceChar :: Char
+                             , nextDir :: Int
+                             } deriving (Show)
 
-data TransitionMapping = TransitionMapping String [Transition] deriving (Show)
-
-charRead :: Transition -> Char
-charRead (Transition x _ _ _) = x
-
-nextState :: Transition -> String
-nextState (Transition _ x _ _) = x
-
-replaceChar :: Transition -> Char
-replaceChar (Transition _ _ x _) = x
-
-nextDir :: Transition -> Int
-nextDir (Transition _ _ _ x) = x
-
-getStateName :: TransitionMapping -> String
-getStateName (TransitionMapping x _) = x
-
-getTransitions :: TransitionMapping -> [Transition]
-getTransitions (TransitionMapping _ x) = x
+data TransitionMapping = TransitionMapping { stateName :: String
+                                           , transitionList :: [Transition]
+                                           } deriving (Show)
 
 -- 0 = OK
 -- 1 = stateName not found
@@ -30,11 +18,11 @@ getTransitions (TransitionMapping _ x) = x
 -- if not 0, the value of Transition doesn't matter
 findTransition :: [TransitionMapping] -> String -> Char -> (Int, Transition)
 findTransition allTrans state c = do
-    let trMapFound = filter (\trMap -> (getStateName trMap) == state) allTrans
+    let trMapFound = filter (\trMap -> (stateName trMap) == state) allTrans
     if length trMapFound == 0 then (1, Transition 'E' "Err" 'E' 0)
     else if length trMapFound > 1 then (11, Transition 'E' "Err" 'E' 0)
     else do
-        let transFound = filter (\trans -> (charRead trans) == c) $ getTransitions $ head trMapFound
+        let transFound = filter (\trans -> (charRead trans) == c) $ transitionList $ head trMapFound
         if length transFound == 0 then (2, Transition 'E' "Err" 'E' 0)
         else if length transFound > 1 then (21, Transition 'E' "Err" 'E' 0)
         else (0, head transFound)
