d378e635d3061761ae2100de8218ad44618ed9f7
pouet <git@mesoule.tamere>
2024-07-26 10:32:06 +0200

    push script python v0.1

---
+++ bonus/curveDrawer.py
@@ -0,0 +1,18 @@
+import csv
+import matplotlib.pyplot as plt
+
+
+def draw():
+    x = []
+    y = []
+    with open("timeCompl.csv", "r", newline="") as io:
+        csvParsing = list(csv.reader(io))
+        for a, b in csvParsing:
+            x.append(a)
+            y.append(b)
+    plt.plot(x, y, "b")
+    plt.savefig("graph.png")
+
+
+if __name__ == "__main__":
+    draw()
